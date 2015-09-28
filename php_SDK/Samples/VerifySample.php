d9cc36fbaf6ef3b491d1ed083299c10e59c05adc
agustinbreit <agustinbreit@gmail.com>
2015-09-28 12:22:21 -0300

    Access token for Stage

--- php_SDK/Samples/VerifySample.php
+++ php_SDK/Samples/VerifySample.php
@@ -9,7 +9,7 @@ $path_to_IncludeClases="../com/payhub/ws/extra/includeClasses.php";
 include_once $path_to_IncludeClases;
 //Defining the Web Service URL
 $WsURL="https://staging-api.payhub.com/api/v2/";
-$oauth_token = "107d74ab-4a18-4713-88ff-69bd05710086";
+$oauth_token = "bb96358e-2aa8-4c6c-8a2e-901b676e979d";
 
 //Defining data for the SALE transaction
 // Merchant data (obtained from the payHub Virtual Terminal (3rd party integration)
