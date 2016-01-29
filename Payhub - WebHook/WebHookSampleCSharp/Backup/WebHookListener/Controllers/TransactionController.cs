b17d1b2eac224d0f7458aa0566b36ac9bdafd68e
Javier <javieru2003@gmail.com>
2016-01-29 16:47:28 -0300

    Adding C# Sample

---
+++ Payhub - WebHook/WebHookSampleCSharp/Backup/WebHookListener/Controllers/TransactionController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebHookListener.Models;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace WebHookListener.Controllers
+{
+    public class TransactionController : ApiController
+    {
+        [HttpPost]
+        public HttpResponseMessage Post(JObject jsonData)
+        {
+            try
+            {
+               // dynamic json = jsonData;
+               /* JObject jTransaction = json.transaction;
+
+                var transaction = jTransaction.ToObject<Transaction>();
+                var response = Request.CreateResponse<Transaction>(System.Net.HttpStatusCode.Created, transaction);
+                string transactionText = "";
+                transactionText = "Transaction Id: " + transaction.transactionId + 
+                    " Transaction Type: " + transaction.transactionType + "."+
+                    " Card Obscured: " + transaction.cardObscured + "."+
+                    " Response Text: " + transaction.responseText + "."+
+                    " Source: " + transaction.source + "."+
+                    " Org Id: " + transaction.orgId + "."+
+                    " Response Code: " + transaction.responseCode + ".";*/
+
+                string path = @"C:\Transactions.txt";
+                // This text is added only once to the file.
+                if (!File.Exists(path))
+                {
+                    // Create a file to write to.
+                    using (StreamWriter sw = File.CreateText(path))
+                    {
+                        sw.WriteLine(jsonData);
+                    }
+                }
+
+                else
+                {
+                    using (StreamWriter sw = File.AppendText(path))
+                    {
+                        sw.WriteLine(jsonData);
+                    }
+                }
+               	
+                var response = Request.CreateResponse<JObject>(System.Net.HttpStatusCode.Created, jsonData);
+                return response;
+            }
+            catch (Exception e)
+            {
+                HttpStatusCode badRequest= HttpStatusCode.BadRequest;
+                return new HttpResponseMessage(badRequest);
+            }
+            
+        }
+
+    }
+}
---
+++ Payhub - WebHook/WebHookSampleCSharp/WebHookListener/Models/Transaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebHookListener.Models
+{
+    public class Transaction
+    {
+        public string transactionType { get; set; }
+
+        public string recurringBillId { get; set; }
+
+        public string cardObscured { get; set; }
+
+        public string responseText { get; set; }
+
+        public string source { get; set; }
+
+        public string orgId { get; set; }
+
+        public string transactionId { get; set; }
+
+        public string responseCode { get; set; }
+    }
+}
